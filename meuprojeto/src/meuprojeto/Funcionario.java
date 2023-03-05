fb862ea5ce31af34579039addb875746a33bc16f
Nayara Nasaria Soares <nayaranasariasoares@gmail.com>
2023-03-05 20:12:15 -0300

    Curso Java Bradesco

---
+++ meuprojeto/src/meuprojeto/Funcionario.java
@@ -0,0 +1,28 @@
+
+package meuprojeto;
+
+
+public class Funcionario {
+
+    private String nome;
+    private boolean ativo;
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+    
+    
+    
+}
---
+++ meuprojeto/src/meuprojeto/Matematica.java
@@ -0,0 +1,13 @@
+
+package meuprojeto;
+
+
+public class Matematica {
+    int maior (int um, int dois){
+        if(um > dois){
+            return um;
+        }else{
+            return dois;
+        }
+    }
+}
